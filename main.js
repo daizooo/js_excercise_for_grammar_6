8531ec9acf6d0f758686fad14e95745d41f7b110
overflow326 <s.daizou@outlook.com>
2019-02-18 22:39:51 +0900

    review_1

--- main.js
+++ main.js
@@ -29,35 +29,6 @@
 //           - 第1引数で受けとった `message` の内容を `alert` を使ってアラートダイアログに表示する
 //   - kadai_3関数を実装した直後に「kadai_3(数値, コールバック関数)」を呼び出して、アラートダイアログのメッセージ内容が意図通りであることを確認する
 
-const kadai_3 = (age, callback) => {
-  let message;
-  if (typeof age !== `number`) {
-    message = "数値が入力されていません";
-  } else if (age >= 20) {
-    message = "数値は20以上です";
-  } else if (age >= 10) {
-    message = "数値は10以上20未満です";
-  } else {
-    message = "値は10未満です";
-  }
-  callback(message);
-};
-
-kadai_3("文字列", function (message) {
-  alert(message);
-});
-kadai_3(25, function (message) {
-  alert(message);
-});
-kadai_3(15, function (message) {
-  alert(message);
-});
-kadai_3(5, function (message) {
-  alert(message);
-});
-kadai_3(true, function (message) {
-  alert(message);
-});
 
 
 // 課題4: 以下の条件を満たす即時関数を作る
@@ -66,7 +37,3 @@ kadai_3(true, function (message) {
 //     - 第2引数: y => 数値
 //   - 処理内容:
 //     - 第1引数のx, 第2引数のyを使って足し算した結果(「x + y」の結果)をconsole.logで出力する。
-
-(function (x, y) {
-  console.log(x + y);
-}(1, 2));

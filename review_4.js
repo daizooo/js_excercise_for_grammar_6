53b37753ef26bfafca2f321848cd494ea7e45341
overflow326 <s.daizou@outlook.com>
2019-02-24 10:55:26 +0900

    review_4

--- review_4.js
+++ review_4.js
@@ -1,25 +1,26 @@
-const kadai_3 = (age, callback) => {
-  if (typeof age !== `number`) {
-    message = "数値が入力されていません";
-  } else if (age >= 20) {
-    message = "値は20以上です";
-  } else if (age >= 10) {
-    message = "値は20以上10未満です";
-  } else {
-    message = "値は10未満です";
-  }
-  callback(message);
-};
+const array = ["金", "銀", "白", "黒"];
+let color;
 
-kadai_3("もじ", (message) => {
-  alert(message);
-});
-kadai_3(26, (message) => {
-  alert(message);
-});
-kadai_3(15, (message) => {
-  alert(message);
-});
-kadai_3(5, (message) => {
-  alert(message);
-});
+function kadai_3(color) {
+  if (color) {
+    if (color === array[0]) {
+      alert("派手やね~");
+      return;
+    } else if (color === array[1]) {
+      alert("シルバー俺も好きよ");
+      return;
+    } else if (color === array[2]) {
+      alert("白Tはマストね");
+      return;
+    } else if (color === array[3]) {
+      alert("毎日全身黒でいいぐらい好き");
+      return;
+    } else {
+      alert(`「${array}」以外の入力には対応しておりません`);
+    }
+  }
+  const input = prompt(`「${array}」の中で好きな色は何？`);
+  kadai_3(input);
+}
+
+kadai_3();
